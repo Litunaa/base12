225382163ade30452aec6643e638c396ea01101b
Litunaa <nubtem1337@gmail.com>
2023-10-17 14:34:01 +0200

    walla den funkar nu

--- conversion.java
+++ conversion.java
@@ -1,7 +1,7 @@
 public class conversion{
     public static void main(String[] args){
 
-        int num = 765756;
+        int num = 12736812;
         StringBuilder numbase12 = new StringBuilder();
         int rest;
         int res;
@@ -9,36 +9,23 @@ public class conversion{
         while (true){
             res = num / 12;
             rest = num % 12;
-            numbase12.insert(0, rest);
+            if (rest == 10){
+                numbase12.insert(0, 'A');
+            }
+            else if (rest == 11){
+                numbase12.insert(0, 'B');
+            }
+            else{
+                numbase12.insert(0, rest);
+            }
+
             num = res;
 
             if (res == 0){
-
                 break;
             }
         }
-
-        int len = numbase12.length();
+        
         System.out.println(numbase12);
-        StringBuilder newnumbase12 = new StringBuilder();
-
-        for (int i = 0; i <= len - 2; i++){
-            if ((numbase12.charAt(i) + "" + numbase12.charAt(i+1)).equals("11")){
-                newnumbase12.append("B");
-                i++;
-            }
-            else if ((numbase12.charAt(i) + "" + numbase12.charAt(i+1)).equals("10")){
-                newnumbase12.append("A");
-                i++;
-            }
-            else {
-                newnumbase12.append(numbase12.charAt(i));
-            }
-            
-        }
-        if ((newnumbase12.charAt(newnumbase12.length() - 1) != 'A') && (newnumbase12.charAt(newnumbase12.length() - 1) != 'B')){
-            newnumbase12.append(numbase12.charAt(numbase12.length()-1));
-        }
-        System.out.println(newnumbase12);
     }
 }
